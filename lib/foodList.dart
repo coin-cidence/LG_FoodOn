d550832758bc5e80870ec629f4cd0edcb025c8be
huunji <ansguswl9684@gmail.com>
2024-11-26 12:18:34 +0900

    foodList.dart 템플릿 완성 by.현지

--- lib/foodList.dart
+++ lib/foodList.dart
@@ -1,82 +1,132 @@
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
 import 'info_dialog.dart';
 
 class FoodListPage extends StatefulWidget {
-  final String shelfName; // 선택된 선반 이름
+  final String shelfSerial; // 스마트 선반 시리얼 번호 전달
 
-  FoodListPage({required this.shelfName}); // 생성자에서 선반 이름 전달받음
+  FoodListPage({required this.shelfSerial});
 
   @override
   _FoodListPageState createState() => _FoodListPageState();
 }
 
 class _FoodListPageState extends State<FoodListPage> {
-  late String selectedShelf; // 현재 선택된 선반 이름
+  late String selectedShelfSerial; // 선택된 선반 시리얼 번호
+  late String selectedShelf;       // DropdownButton에서 사용할 선택된 선반
+  List<Map<String, dynamic>> foodData = []; // 조회된 식품 데이터를 저장
+
   bool _isInfoVisible = false; // 정보창 표시 여부
   GlobalKey infoButtonKey = GlobalKey(); // Key for info button to find position
 
   @override
   void initState() {
     super.initState();
-    selectedShelf = widget.shelfName; // 초기값으로 전달받은 선반 이름 설정
+    selectedShelfSerial = widget.shelfSerial;
+
+    // DropdownButton 초기값 설정
+    final shelvesData = DummyData.getSmartShelvesData();
+    final shelfData = shelvesData.firstWhere(
+          (shelf) => shelf['smartShelfSerial'] == selectedShelfSerial,
+      orElse: () => {'shelfLocation': '선반 1'}, // 기본값
+    );
+    selectedShelf = shelfData['shelfLocation'];
+
+    // 더미 데이터 조회
+    fetchFoodData(selectedShelfSerial);
   }
 
-  // 강조 표시할 좌표 정보
-  final List<List<int>> highlightedCoordinates = [
-    [3, 2], [3, 3], [3, 4], [3, 5],
-    [4, 1], [4, 2], [4, 3], [4, 4], [4, 5],
-    [5, 2], [5, 3], [5, 4],
-  ];
+  // 더미 데이터 조회 함수
+  void fetchFoodData(String shelfSerial) async {
+    final allFoodData = DummyData.getFoodManagementData();
+    final List<Map<String, dynamic>> fetchedData = allFoodData
+        .where((food) => food['smartShelfSerial'] == shelfSerial)
+        .toList();
+
+    setState(() {
+      foodData = fetchedData;
+    });
+
+    if (fetchedData.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("데이터 없음"),
+            content: Text("선택한 선반에 식품 정보가 없습니다."),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text("확인"),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  double _calculateInfoDialogPosition() {
+    final RenderBox? box = infoButtonKey.currentContext?.findRenderObject() as RenderBox?;
+    if (box != null) {
+      final Offset position = box.localToGlobal(Offset.zero);
+      return position.dy + box.size.height; // 아이콘 바로 아래에 위치 설정
+    }
+    return 95; // 위치를 찾을 수 없을 경우 기본값
+  }
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
 
-    // 화면 너비의 80%를 차지하도록 설정
     final gridWidth = screenWidth * 0.8;
-    // 가로 셀 개수
-    final columns = 11;
-    // 각 셀의 크기 (1:1 비율을 유지하기 위해 gridWidth를 기준으로 계산)
+    final columns = 12;
     final cellSize = gridWidth / columns;
 
     return Scaffold(
       body: Stack(
         children: [
-          // 배경 이미지 설정
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('images/FTIE_엘지배경_대지.png'), // 배경 이미지 경로
-                fit: BoxFit.cover, // 이미지가 화면 전체를 덮도록 설정
+                image: AssetImage('images/FTIE_엘지배경_대지.png'),
+                fit: BoxFit.cover,
               ),
             ),
           ),
           Column(
             children: [
-              SizedBox(height: MediaQuery.of(context).padding.top), // 상태바 공간
+              // 앱바 영역
+              SizedBox(height: MediaQuery.of(context).padding.top),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: () => Navigator.pop(context),
+                    // 왼쪽 정렬: Back 버튼과 Dropdown
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        DropdownButton<String>(
+                          value: selectedShelf,
+                          items: DummyData.getSmartShelvesData().map((shelf) {
+                            return DropdownMenuItem<String>(
+                              value: shelf['shelfLocation'],
+                              child: Text(shelf['shelfLocation']),
+                            );
+                          }).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              selectedShelf = value!;
+                            });
+                          },
+                        ),
+                      ],
                     ),
-                    DropdownButton<String>(
-                      value: selectedShelf,
-                      items: ["선반 1", "선반 2", "선반 3", "선반 4"].map((item) {
-                        return DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(item),
-                        );
-                      }).toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          selectedShelf = value!;
-                        });
-                      },
-                    ),
+                    Spacer(), // 왼쪽과 오른쪽 간격 조정
+                    // 오른쪽 정렬: Search와 Info 아이콘
                     Row(
                       children: [
                         IconButton(
@@ -84,14 +134,14 @@ class _FoodListPageState extends State<FoodListPage> {
                           onPressed: () {},
                         ),
                         IconButton(
-                          key: infoButtonKey, // Assign key for positioning
+                          key: infoButtonKey,
                           icon: Icon(Icons.info),
                           onPressed: () {
                             final RenderBox box = infoButtonKey.currentContext!
                                 .findRenderObject() as RenderBox;
                             final Offset position = box.localToGlobal(Offset.zero);
                             setState(() {
-                              _isInfoVisible = !_isInfoVisible; // Toggle visibility
+                              _isInfoVisible = !_isInfoVisible;
                             });
                           },
                         ),
@@ -100,21 +150,39 @@ class _FoodListPageState extends State<FoodListPage> {
                   ],
                 ),
               ),
+
+              // 그리드 영역
               Expanded(
                 flex: 6, // 상단 영역: 60% 높이
                 child: Center(
                   child: Container(
-                    width: gridWidth, // 그리드가 화면의 80% 너비를 차지
+                    width: gridWidth,
                     child: Stack(
-                      children: List.generate(11 * 17, (index) {
+                      children: List.generate(12 * 17, (index) {
                         final row = index ~/ columns; // 세로 (행)
                         final column = index % columns; // 가로 (열)
 
-                        // 강조 표시 여부 확인
-                        final isHighlighted = highlightedCoordinates.any(
-                              (coordinate) =>
-                          coordinate[0] == row && coordinate[1] == column,
+                        final cellData = foodData.firstWhere(
+                              (food) => food['foodLocation'].any(
+                                      (location) => location['x'] == row && location['y'] == column),
+                          orElse: () => <String, dynamic>{},
                         );
+
+                        // 셀 색상 결정 로직
+                        Color cellColor = Colors.white; // 기본값
+                        if (cellData.isNotEmpty) {
+                          final isUnused = DateTime.now().difference(
+                            DateTime.parse(cellData['foodWeightUpdateTime']),
+                          ).inDays >=
+                              cellData['foodUnusedNotifPeriod'];
+                          if (isUnused) {
+                            cellColor = Color(0xFFF0D0FF); // 보라색
+                          } else if (cellData['foodWeight'] != 0) {
+                            cellColor = Color(0xFFB4E0FF); // 파란색
+                          } else {
+                            cellColor = Color(0xFFFFA68B); // 빨간색
+                          }
+                        }
 
                         return Positioned(
                           left: column * cellSize, // 열 위치
@@ -123,9 +191,7 @@ class _FoodListPageState extends State<FoodListPage> {
                             width: cellSize * 0.9, // 셀 크기 (간격 포함, 90%)
                             height: cellSize * 0.9, // 셀 크기 (간격 포함, 90%)
                             decoration: BoxDecoration(
-                              color: isHighlighted
-                                  ? Colors.purple[200]
-                                  : Colors.white,
+                              color: cellColor,
                               borderRadius: BorderRadius.circular(5),
                             ),
                           ),
@@ -135,54 +201,101 @@ class _FoodListPageState extends State<FoodListPage> {
                   ),
                 ),
               ),
+
+              // 리스트 영역
+              // 리스트 영역
               Divider(height: 1, color: Colors.grey), // 구분선
               Expanded(
                 flex: 4, // 하단 영역: 40% 높이
-                child: ListView(
-                  padding: EdgeInsets.all(8.0),
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.purple[200],
-                        radius: 10,
-                      ),
-                      title: Text('가지'),
-                      trailing: Icon(Icons.arrow_forward_ios, size: 16),
+                child: Center(
+                  child: Container(
+                    width: screenWidth * 0.8, // 리스트 영역의 너비: 화면의 90%로 설정
+                    child: ListView.builder(
+                      padding: EdgeInsets.all(8.0),
+                      itemCount: foodData.length,
+                      itemBuilder: (context, index) {
+                        final food = foodData[index];
+
+                        // 색상 결정 로직
+                        Color cellColor = Colors.white; // 기본값
+                        final isUnused = DateTime.now().difference(
+                          DateTime.parse(food['foodWeightUpdateTime']),
+                        ).inDays >= food['foodUnusedNotifPeriod'];
+                        if (isUnused) {
+                          cellColor = Color(0xFFF0D0FF); // 보라색
+                        } else if (food['foodWeight'] != 0) {
+                          cellColor = Color(0xFFB4E0FF); // 파란색
+                        } else {
+                          cellColor = Color(0xFFFFA68B); // 빨간색
+                        }
+
+                        return Container(
+                          width: double.infinity,
+                          height: 55,
+                          margin: const EdgeInsets.symmetric(vertical: 10), // 리스트 간 간격
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              // 색상 박스
+                              Container(
+                                width: 30,
+                                height: 30,
+                                decoration: ShapeDecoration(
+                                  color: cellColor,
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                                ),
+                              ),
+                              // 식품 이름
+                              Expanded(
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                                  child: Text(
+                                    food['foodName'],
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontFamily: 'LG EI Text TTF',
+                                      fontWeight: FontWeight.w400,
+                                      letterSpacing: -0.8,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              // 화살표 아이콘
+                              Icon(
+                                Icons.arrow_back_ios,
+                                size: 16,
+                                color: Colors.grey,
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                    ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.blue[200],
-                        radius: 10,
-                      ),
-                      title: Text('감'),
-                      trailing: Icon(Icons.arrow_forward_ios, size: 16),
-                    ),
-                    ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.blue[300],
-                        radius: 10,
-                      ),
-                      title: Text('방울토마토'),
-                      trailing: Icon(Icons.arrow_forward_ios, size: 16),
-                    ),
-                    ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.purple[100],
-                        radius: 10,
-                      ),
-                      title: Text('사과'),
-                      trailing: Icon(Icons.arrow_forward_ios, size: 16),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ],
           ),
-          // 정보창
+
           if (_isInfoVisible)
             InfoDialog(
               xPosition: MediaQuery.of(context).size.width / 2,
-              yPosition: 75, // Adjusted for testing
+              yPosition: _calculateInfoDialogPosition(),
             ),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
 import 'foodList.dart'; // FoodListPage가 정의된 파일 import
 
 void main() {
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // 디버그 배너 숨기기
       home: MyHomePage(), // 메인 화면 설정
     );
   }
@@ -18,59 +20,35 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final shelvesData = DummyData.getSmartShelvesData();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('메인 화면'),
+        title: Text('스마트 선반 관리'), // 앱바 제목
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FoodListPage(shelfName: '선반 1'),
-                  ),
-                );
-              },
-              child: Text('선반 1'), // 버튼 텍스트
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FoodListPage(shelfName: '선반 2'),
-                  ),
-                );
-              },
-              child: Text('선반 2'), // 버튼 텍스트
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FoodListPage(shelfName: '선반 3'),
-                  ),
-                );
-              },
-              child: Text('선반 3'), // 버튼 텍스트
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FoodListPage(shelfName: '선반 4'),
-                  ),
-                );
-              },
-              child: Text('선반 4'), // 버튼 텍스트
-            ),
-          ],
+        child: ListView.builder(
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          itemCount: shelvesData.length,
+          itemBuilder: (context, index) {
+            final shelf = shelvesData[index];
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FoodListPage(
+                        shelfSerial: shelf['smartShelfSerial'],
+                      ),
+                    ),
+                  );
+                },
+                child: Text(shelf['shelfLocation']), // 버튼 텍스트
+              ),
+            );
+          },
         ),
       ),
     );
